099f47942ac2e6d8662cd62e82c4838ee3417fa7
vanilija <digitalni2k@gmail.com>
2016-03-03 22:57:44 +0100

    Initial commit - added project files

---
+++ alphadrina/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alphadrina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+        public ActionResult Transportation()
+        {
+            return View();
+        }
+
+        public ActionResult Notice()
+        {
+            return View("~/Views/Home/Notice.cshtml");
+        }
+    }
+}
---
+++ alphadrina/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(alphadrina.Startup))]
+namespace alphadrina
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
